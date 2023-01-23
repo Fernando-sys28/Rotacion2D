00e5a47eed8ef7bc104e6036b063fca884e64e95
ferch <ferch@LAPTOP-L0V0GDQG>
2023-01-22 20:04:22 -0600

    Agregar archivos de proyecto.

---
+++ doslineas/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace doslineas
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.canvas = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.canvas.Location = new System.Drawing.Point(107, 63);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(700, 432);
+            this.canvas.TabIndex = 0;
+            this.canvas.TabStop = false;
+            this.canvas.Click += new System.EventHandler(this.BCT_CANVAS_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(41, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Cuadrado";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(313, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 31);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(469, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 34);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "0,0";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(245, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Angle:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(832, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "171707";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(594, 23);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(192, 34);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Centro figura";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(792, 23);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 34);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "CPantalla";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 524);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.canvas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PictureBox canvas;
+        private Button button1;
+        private TextBox textBox1;
+        private Button button2;
+        private Label label1;
+        private Label label2;
+        private Button button3;
+        private Button button4;
+    }
+}
---
+++ doslineas/Form1.cs
@@ -0,0 +1,178 @@
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms;
+
+namespace doslineas
+{
+    public partial class Form1 : Form
+    {
+        Graphics g,ct;
+        Bitmap bmp,center;
+        static Point  a, b, c, d;
+        private bool Boton4 = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(canvas.Width, canvas.Height);
+            g = Graphics.FromImage(bmp);
+            canvas.Image = bmp;
+
+            center = new Bitmap(Width,Height);
+            ct = Graphics.FromImage(center);
+        }
+
+        private void BCT_CANVAS_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            a = new Point(canvas.Width / 2, 0);
+            b = new Point(canvas.Width / 2, canvas.Height);
+            c = new Point(0, canvas.Height / 2);
+            d = new Point(canvas.Width, canvas.Height / 2);
+            g.DrawLine(Pens.Gray, a, b);
+            g.DrawLine(Pens.Gray, c, d);
+
+            int angle;
+
+            a = new Point(-50, 50);
+            b = new Point(50, 50);
+            c = new Point(50, -50);
+            d = new Point(-50, -50);
+
+            angle = int.Parse(textBox1.Text);
+
+            Render(a, b, angle);
+            Render(b, c, angle);
+            Render(c, d, angle);
+            Render(d, a, angle);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Boton4 = true;
+            a = new Point(canvas.Width / 2, 0);
+            b = new Point(canvas.Width / 2, canvas.Height);
+            c = new Point(0, canvas.Height / 2);
+            d = new Point(canvas.Width, canvas.Height / 2);
+            g.DrawLine(Pens.Gray, a, b);
+            g.DrawLine(Pens.Gray, c, d);
+
+            int angle;
+
+            a = new Point(0,0);
+            b = new Point(0,100);
+            c = new Point(100, 100);
+            d = new Point(100, 0);
+
+            angle = int.Parse(textBox1.Text);
+
+            Render(a, b, angle);
+            Render(b, c, angle);
+            Render(c, d, angle);
+            Render(d, a, angle);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            a = new Point(canvas.Width / 2,0);
+            b = new Point(canvas.Width / 2, canvas.Height);
+            c = new Point(0, canvas.Height/2);
+            d = new Point(canvas.Width, canvas.Height/2);
+            g.DrawLine(Pens.Gray, a, b);
+            g.DrawLine(Pens.Gray, c, d);
+
+            int x=canvas.Width/2;
+            int y=canvas.Height/2;
+
+            a = new Point(x, y);
+            b = new Point(x,y-100);
+            c = new Point(x+100, y-100);
+            d = new Point(x+100, y);
+
+            g.DrawLine(Pens.Gray, a, b);
+            g.DrawLine(Pens.Gray, b, c);
+            g.DrawLine(Pens.Gray, c, d);
+            g.DrawLine(Pens.Gray, d, a);
+
+            canvas.Invalidate();
+
+        }
+
+
+        private void Render(Point a, Point b, int angle)
+        {
+            PointF a2, b2;
+            int Sx, Sy;
+
+            if (Boton4 == true)
+            {
+                Sx = (Screen.PrimaryScreen.WorkingArea.Width / 2) - (canvas.Width);
+                Sy = (Screen.PrimaryScreen.WorkingArea.Height / 2 ) - (canvas.Height);
+
+            }else
+                Sx = (canvas.Width/2);
+                Sy = (canvas.Height/2);
+
+            a2 = new PointF(Sx + a.X, Sy - a.Y);
+            b2 = new PointF(Sx + b.X, Sy - b.Y);
+            double degrees = Math.PI * angle / 180.0;
+
+            a2.X = Sx + (float)((a.X * Math.Cos(degrees)) - (a.Y * Math.Sin(degrees)));
+            a2.Y = Sy - (float)((a.X * Math.Sin(degrees)) + (a.Y * Math.Cos(degrees)));
+            b2.X = Sx + (float)((b.X * Math.Cos(degrees)) - (b.Y * Math.Sin(degrees)));
+            b2.Y = Sy - (float)((b.X * Math.Sin(degrees)) + (b.Y * Math.Cos(degrees)));
+
+            g.DrawLine(Pens.White, a2, b2);
+
+            canvas.Refresh();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            a = new Point(canvas.Width / 2, 0);
+            b = new Point(canvas.Width / 2, canvas.Height);
+            c = new Point(0, canvas.Height / 2);
+            d = new Point(canvas.Width, canvas.Height / 2);
+            g.DrawLine(Pens.Gray, a, b);
+            g.DrawLine(Pens.Gray, c, d);
+
+            int angle;
+
+            a = new Point(0,0);
+            b = new Point(0, 100);
+            c = new Point(100, 100);
+            d = new Point(100, 0);
+
+            angle = int.Parse(textBox1.Text);
+
+            Render(a, b, angle);
+            Render(b, c, angle);
+            Render(c, d, angle);
+            Render(d, a, angle);
+
+            
+        }
+
+
+    }
+}
